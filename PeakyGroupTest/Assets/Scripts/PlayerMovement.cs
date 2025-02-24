ae803384f32cd348b4278f94240df57cdc2bebb4
kankrmnlv <kkanigaming@gmail.com>
2025-02-24 17:52:05 +0800

    added movement boundaries - scalable solution

--- PeakyGroupTest/Assets/Scripts/PlayerMovement.cs
+++ PeakyGroupTest/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,9 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //movement
     public float moveSpeed;
 
     private CharacterController characterController;
@@ -10,9 +12,13 @@ public class PlayerMovement : MonoBehaviour
 
     private float horizontalInput;
     private float verticalInput;
+
+    //boundarie variables
+    private float maxXpos, minXpos, maxZpos, minZpos;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        CalculateMovementBoundaries();
     }
 
     private void Update()
@@ -27,11 +33,33 @@ public class PlayerMovement : MonoBehaviour
         }
 
         characterController.Move(movement * moveSpeed * Time.deltaTime);
+
+        SetMovementBoundaries();
     }
 
     private void GetInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void SetMovementBoundaries()
+    {
+        //Sets movement boundaries
+        float clampedX = Mathf.Clamp(transform.position.x, minXpos, maxXpos);
+        float clampedZ = Mathf.Clamp(transform.position.z, minZpos, maxZpos);
+
+        transform.position = new Vector3(clampedX, transform.position.y, clampedZ);
+    }
+
+    private void CalculateMovementBoundaries()
+    {
+        //Calculates the ground size
+        Vector3 groundSize = GameObject.FindWithTag("Ground").GetComponent<Renderer>().bounds.size;
+
+        maxXpos = groundSize.x / 2f;
+        minXpos = -groundSize.x / 2f;
+        maxZpos = groundSize.z / 2f;
+        minZpos = -groundSize.z / 2f;
     }
 }
